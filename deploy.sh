599ecccda16d34332b0307f8415c300918b88521
Michael Youngdahl <Michael.Youngdahl@ibm.com>
2018-06-28 15:36:53 -0400

    fixed CreateGrantee so it works now

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,13 @@
 npm run prepublish
-composer network install -c PeerAdmin@hlfv1 -a dist/ed-grants-alpha.bna
-composer network start -A admin -S adminpw -c PeerAdmin@hlfv1 -f admin@ed-grants-alpha.card -n ed-grants-alpha -V 0.0.1
-composer card import --file admin@ed-grants-alpha.card
-composer network ping -c admin@ed-grants-alpha
+composer network install -c PeerAdmin@hlfv1 -a dist/grantblock.bna
+composer network start -A admin -S adminpw -c PeerAdmin@hlfv1 -f admin@grantblock.card -n grantblock -V 0.0.1
+composer card import --file admin@grantblock.card
+composer network ping -c admin@grantblock
 
 #echo "Starting composer rest server... "
-#composer-rest-server -c admin@ed-grants-alpha
+#composer-rest-server -c admin@grantblock
 
 #if [ "$1" == "-c" ]; then
 #	echo "Starting with composer server..."
-#	composer-rest-server -p hlfv1 -n ed-grants-alpha -i PeerAdmin -s randomStr -N always
+#	composer-rest-server -p hlfv1 -n grantblock -i PeerAdmin -s randomStr -N always
 #fi
